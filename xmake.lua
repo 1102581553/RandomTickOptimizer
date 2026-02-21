658b7d57c78ec34e7e3d57290f0ec7181cbea7bd
1102581553 <pheyeji@gmail.com>
2026-02-21 13:36:35 +0000

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -9,12 +9,15 @@ option("target_type")
 option_end()
 
 add_requires("levilamina", {configs = {target_type = get_config("target_type")}})
+add_requires("levibuildscript")  -- 用于自动链接和打包
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
 
 target("RandomTickOptimizer")
+    add_rules("@levibuildscript/linkrule")
+    add_rules("@levibuildscript/modpacker")
     add_cxflags("/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
     add_defines("NOMINMAX", "UNICODE")
     add_packages("levilamina")
@@ -25,3 +28,8 @@ target("RandomTickOptimizer")
     add_headerfiles("src/*.h")
     add_files("src/*.cpp")
     add_includedirs("src")
+    if is_config("target_type", "server") then
+        -- 服务端特有源码（如果有）
+    else
+        -- 客户端特有源码（如果有）
+    end
